e35d0f1f344c10c45085c2761fb858e7db265c8c
danielschmetterling <daniel.schmetterling@proton.me>
2026-03-21 16:03:54 -0300

    teste básico de calculadora

--- .java/prática/CalculadoraElementar.java
+++ .java/prática/CalculadoraElementar.java
@@ -1,3 +1,35 @@
 public class CalculadoraElementar {
-    
+    public double valor1;
+    public double valor2;
+    public double resultado;
+
+    // public CalculadoraElementar(double v1, double v2){
+    //     valor1 = 0;
+    //     valor2 = 0;
+    //     resultado = 0;
+    // } Não foi necessário para essa prática.
+
+    public void somar(){
+        resultado = valor1 + valor2;
+    }
+    public void subtrair(){
+        resultado = valor1 - valor2;
+    }
+    public void mostrarResultado(){
+        System.out.println(resultado);
+    }
+
+    public static void main(String[] args) {
+        CalculadoraElementar calc = new CalculadoraElementar();
+        calc.valor1 = 20;
+        calc.valor2 = 10;
+
+        System.out.println("Soma:");
+        calc.somar();
+        calc.mostrarResultado();
+
+        System.out.println("Subtração:");
+        calc.subtrair();
+        calc.mostrarResultado();
+    }
 }
