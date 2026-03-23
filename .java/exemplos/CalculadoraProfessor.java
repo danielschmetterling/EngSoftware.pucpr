4d13194b48f06c297e324ee3f26b36cee3190809
Niel <132403949+danielschmetterling@users.noreply.github.com>
2026-03-23 09:03:40 -0300

    ainda tem mais

--- .java/exemplos/CalculadoraProfessor.java
+++ .java/exemplos/CalculadoraProfessor.java
@@ -4,6 +4,10 @@ public class Calculadora {
     private double valor2;
     private double resultado;
 
+    private void setarValor1AposOperacao(){
+        valor1 = resultado;
+    }
+
     public Calculadora(double v1, double v2){
         valor1 = v1;
         valor2 = v2;
@@ -12,10 +16,14 @@ public class Calculadora {
 
     public void somar(){
         resultado = valor1 + valor2;
+        setarValor1AposOperacao();
     }
+
     public void subtrair(){
         resultado = valor1 - valor2;
+        setarValor1AposOperacao();
     }
+
     public void mostrar(){
         System.out.println(resultado);
     }
@@ -27,11 +35,15 @@ public class Calculadora {
     // }
 
     public static void main(String[] args){
-        Calculadora c = new Calculadora(10.0, 15.0);
-        c.somar();
-        c.mostrar();
-        c.subtrair();
-        c.mostrar();
+        Calculadora c1 = new Calculadora(10.0, 15.0);
+        Calculadora b1 = new Calculadora(10.0, 35.0);
+        c1.somar();
+        c1.mostrar();
+        c1.subtrair();
+        c1.mostrar();
+        
+        b1.subtrair();
+        b1.mostrar();
     }
 
 }
