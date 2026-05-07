ea14e277ac2903aa1f062711d2a6407b49b1b92d
Niel <132403949+danielschmetterling@users.noreply.github.com>
2026-05-07 11:18:06 -0300

    Implement area calculation for shapes in Program class
    Updated listarFormas method to print area of shapes and added example shapes in main method.

--- Programação Orientada a Objetos/Calcular Areas/src/Program.java
+++ Programação Orientada a Objetos/Calcular Areas/src/Program.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+
 public class Program {
     public static void listarFormas(ArrayList<Forma> l){
-        for (Forma p : 1){
-
+        for (Forma f : l){
+            System.out.println(f.calcularArea());
         }
     }
 
     public static void main(String[] args){
+//        Forma circulo = new Circulo(20);
+//        Forma retangulo = new Retangulo(5,4);
 
+
+        ArrayList<Forma> lista = new ArrayList<>();
+        lista.add(new Circulo(20));
+        lista.add(new Retangulo(5,4));
+        listarFormas(lista);
     }
 }
